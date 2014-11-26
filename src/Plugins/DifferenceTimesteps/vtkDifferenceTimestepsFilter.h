948ac6699de8555a8aca94ccaeac3e64f15fc444
rnv <rnv@opencascade.com>
2014-11-26 13:33:26 +0300

    Create plugin as a filter which allows analyze difference between two time steps of one data array (field).
    Added the following server manager properties:
    1. Property is responsible for enumeration the scalar array names of input data set;
    2. Property is responsible for specifying the first time step;
    3. Property is responsible for specifying the second time step;
    4. Property is responsible for specifying the prefix of new array name.
    
    Filter is capable of reading various data objects.
    Also filter can return the corresponding errors if the input data are not correct or property of user interface controls is invalid. This filter appointed into "Temporal" group of filters.
    Added the icon to this filter.

---
+++ src/Plugins/DifferenceTimesteps/vtkDifferenceTimestepsFilter.h
@@ -0,0 +1,148 @@
+// Copyright (C) 2014  CEA/DEN, EDF R&D
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+//
+// See http://www.salome-platform.org/ or email : webmaster.salome@opencascade.com
+//
+// Author : Maxim Glibin
+
+#ifndef __DifferenceTimestepsFilter_h_
+#define __DifferenceTimestepsFilter_h_
+
+#include <vtkMultiTimeStepAlgorithm.h>
+
+#include <vector>
+
+class vtkDataSet;
+class vtkStringArray;
+
+/**
+ * Description of class:
+ * Class allows to compute difference between two time steps of one data array (field).
+*/
+class VTK_EXPORT vtkDifferenceTimestepsFilter : public vtkMultiTimeStepAlgorithm
+{
+public:
+  /// Returns pointer on a new instance of the class
+  static vtkDifferenceTimestepsFilter* New();
+
+  vtkTypeMacro(vtkDifferenceTimestepsFilter, vtkMultiTimeStepAlgorithm);
+
+  /// Prints current state of the objects
+  virtual void PrintSelf(ostream &, vtkIndent);
+
+  // Description:
+  // Set/Get methods for first time step.
+  vtkSetMacro(FirstTimeStepIndex, int);
+  vtkGetMacro(FirstTimeStepIndex, int);
+
+  // Description:
+  // Set/Get methods for first time step.
+  vtkSetMacro(SecondTimeStepIndex, int);
+  vtkGetMacro(SecondTimeStepIndex, int);
+
+  // Description:
+  // Get methods for range of indices of time steps.
+  vtkGetVector2Macro(RangeIndicesTimeSteps, int);
+
+  // Description:
+  // Set/Get methods for prefix of array name.
+  vtkSetStringMacro(ArrayNamePrefix);
+  vtkGetStringMacro(ArrayNamePrefix);
+
+protected:
+  /// Constructor & destructor
+  vtkDifferenceTimestepsFilter();
+  virtual ~vtkDifferenceTimestepsFilter();
+
+  /// The methods which is called on filtering data
+  virtual int FillInputPortInformation(int, vtkInformation *);
+
+  virtual int FillOutputPortInformation(int, vtkInformation *);
+
+  virtual int RequestDataObject(vtkInformation *,
+                                vtkInformationVector **,
+                                vtkInformationVector *);
+
+  virtual int RequestInformation(vtkInformation *,
+                                 vtkInformationVector **,
+                                 vtkInformationVector *);
+
+  virtual int RequestUpdateExtent(vtkInformation *,
+                                  vtkInformationVector **,
+                                  vtkInformationVector *);
+
+  virtual int RequestData(vtkInformation *,
+                          vtkInformationVector **,
+                          vtkInformationVector *);
+
+  // Description:
+  // General computation differences routine for any type on input data. This
+  // is called recursively when heirarchical/multiblock data is encountered.
+  vtkDataObject *DifferenceDataObject(vtkDataObject* theInput1,
+                                      vtkDataObject* theInput2);
+
+  // Description:
+  // Root level interpolation for a concrete dataset object.
+  // Point/Cell data and points are different.
+  // Needs improving if connectivity is to be handled.
+  virtual vtkDataSet *DifferenceDataSet(vtkDataSet* theInput1,
+                                        vtkDataSet* theInput2);
+
+  // Description:
+  // Compute difference a single vtkDataArray. Called from computation
+  // of the difference routine on pointdata or celldata.
+  virtual vtkDataArray *DifferenceDataArray(vtkDataArray** theArrays,
+                                            vtkIdType      theN);
+
+  // Description:
+  // Range of indices of the time steps.
+  int RangeIndicesTimeSteps[2];
+  
+  // Description:
+  // First time step index.
+  int FirstTimeStepIndex;
+
+  // Description:
+  // Second time step index.
+  int SecondTimeStepIndex;
+
+  // Description:
+  // Length of time steps array
+  int NumberTimeSteps;
+
+  // Description:
+  // Array of time step values.
+  vtkstd::vector<double> TimeStepValues;
+
+  // Description:
+  // Prefix of array name.
+  char *ArrayNamePrefix;
+
+private:
+  vtkDifferenceTimestepsFilter(const vtkDifferenceTimestepsFilter &); // Not implemented yet
+  void operator=(const vtkDifferenceTimestepsFilter &); // Not implemented yet
+
+  // Description:
+  // Get field association type.
+  int GetInputFieldAssociation();
+
+  // Description:
+  // Called just before computation of the difference of the dataset to ensure that
+  // each data array has the same array name, number of tuples or components and etc.
+  bool VerifyArrays(vtkDataArray **theArrays, int theNumArrays);
+};
+
+#endif // __DifferenceTimestepsFilter_h_
